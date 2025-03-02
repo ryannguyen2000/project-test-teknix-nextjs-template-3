45a3318e4f2b54ecc0beab65ef567c244edce94a
ryannguyen <nguyenthaibao.haugiang@gmail.com>
2025-03-02 16:20:02 +0700

    Update index - Add TableFootball

--- components/commons/index.tsx
+++ components/commons/index.tsx
@@ -12,4 +12,5 @@ export const components = {
   title_complex: dynamic(() => import("./titleComplex")),
   title_header: dynamic(() => import("./titleHeader")),
   title_header_gradient: dynamic(() => import("./titleHeaderGradient")),
+  table_football: dynamic(() => import("./TableFootball")),
 };
